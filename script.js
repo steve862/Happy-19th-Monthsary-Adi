ed6d67aaca8337d6d6b228b8b4ddc7d7af28d9c2
steve862 <87558290+steve862@users.noreply.github.com>
2026-09-10 04:15:34 +0800

    Refactor script.js to remove redundant code

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ const letterAudio = document.getElementById("letterAudio");
 const text =
 `Being with you has always been my choice, but giving up was never my option. In every mistake I made, you were there, as if you were my only best friend and family who cared that I exist in a way that I was accepted. In every obstacle in life, no matter how far you were, you were there for me, patiently guiding me to be a better understanding and righteous person who values peace of mind over a crowded head. If love is a choice, loving you was never a question. 
 
-
 When I'm with you, I feel safe and confident, because with you, only true love still exists. Even though I couldn't reach your hands, kiss your lips, and hug you every time I want, in my heart you always exist; in my dreams I always think of our future, and in my thoughts I always think of you like there is no tomorrow. No matter how far you are, my heart has always been closer to you.
 
 Distance wasn't our choice, but loving you isn't my reason to question myself. Giving up on you was never my word, because love isn't built on certainty, for it's built on the choice to have faith and trust, even when we're far apart. `;
